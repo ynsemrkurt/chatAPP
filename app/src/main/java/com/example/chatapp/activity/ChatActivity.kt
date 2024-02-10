790709d96466843d98e1d8f4e1c8ebe80d488694
Yunus Emre <yunusemre-kurt@outlook.com>
2024-02-10 16:15:01 +0300

    Complated Chat Screen Desing

--- app/src/main/java/com/example/chatapp/activity/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/activity/ChatActivity.kt
@@ -19,29 +19,29 @@ import com.google.firebase.database.getValue
 import de.hdodenhof.circleimageview.CircleImageView
 
 class ChatActivity : AppCompatActivity() {
-    var firebaseUser: FirebaseUser? =null
-    var reference:DatabaseReference?=null
+    var firebaseUser: FirebaseUser? = null
+    var reference: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        var intent=getIntent()
-        var friendUserId=intent.getStringExtra("userId").toString()
+        var intent = getIntent()
+        var friendUserId = intent.getStringExtra("userId").toString()
 
-        firebaseUser=FirebaseAuth.getInstance().currentUser
-        reference=FirebaseDatabase.getInstance().getReference("users").child(friendUserId)
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        reference = FirebaseDatabase.getInstance().getReference("users").child(friendUserId)
 
-        reference!!.addValueEventListener(object :    ValueEventListener{
+        reference!!.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val user=snapshot.getValue(User::class.java)
-                if (user!!.pimage==""){
+                val user = snapshot.getValue(User::class.java)
+                if (user!!.pimage == "") {
                     findViewById<CircleImageView>(R.id.imageFriendProfileImage).setImageResource(R.drawable.profile_image)
+                } else {
+                    Glide.with(this@ChatActivity).load(user.pimage)
+                        .into(findViewById(R.id.imageFriendProfileImage))
                 }
-                else{
-                    Glide.with(this@ChatActivity).load(user.pimage).into(findViewById(R.id.imageFriendProfileImage))
-                }
-                findViewById<TextView>(R.id.textFriendName).text=user.userName
+                findViewById<TextView>(R.id.textFriendName).text = user.userName
             }
 
             override fun onCancelled(error: DatabaseError) {
